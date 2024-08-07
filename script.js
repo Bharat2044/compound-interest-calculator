bd33bad0840ab4972bfac7136f87d35c1f44ee17
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2024-08-07 10:56:40 +0530

    javascript code added

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function calculateReturns() {
+  // Get the values from the input fields
   const capital = document.getElementById("capital").value;
   const years = document.getElementById("years").value;
   const interest = document.getElementById("interest").value;
 
+  // Check if the input fields are not empty and calculate the total amount 
   if (capital !== "" && years !== "" && interest !== "") {
-    const totalAmount = capital * Math.pow(1 + interest / 100, years);
+    const amount = capital * Math.pow(1 + interest / 100, years);
 
-    document.getElementById("result").innerHTML = `${totalAmount.toFixed(2)}`;
+    document.getElementById("totalAmount").innerHTML = `₹ ${amount.toFixed(2)}`;
+    document.getElementById("totalInterest").innerHTML = `₹ ${(amount - capital).toFixed(2)}`;
   } else {
     alert("Please fill all the fields");
   }
